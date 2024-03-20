58b8b290c6a1572b3ed3be1915c1b0b8e963efd9
tubaj@TubaJan <tubajan282@gmail.com>
2024-03-20 17:44:48 +0500

    simple calculator 2

---
+++ main.js
@@ -0,0 +1,27 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter Your First Number", name: "firstNumber", type: "number" },
+    { message: "Enter your Second Number", name: "secondNumber", type: "number" },
+    {
+        message: "Select your Operation",
+        name: "operator",
+        type: "list",
+        choices: ['Addition', 'Subtraction', 'Multiplication', 'Division'],
+    },
+]);
+//Condition part
+if (answer.operator === 'Addition') {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === 'Subtraction') {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === 'Multiplication') {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === 'Division') {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log(`Please select valid options`);
+}
